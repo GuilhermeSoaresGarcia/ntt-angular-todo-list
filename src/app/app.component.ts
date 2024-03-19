617548d40bf2070299cfc0109683c2788348d862
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-18 21:47:54 -0300

    feat: criadas as lógicas para incluir, remover e limpar itens da lista de tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { InputComponent } from './input/input.component'
-import { ResultsComponent } from './results/results.component'
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [InputComponent, ResultsComponent],
-  templateUrl: './app.component.html'  
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ntt-angular-todo-list';
+  title = 'ntt-angular-todo-list'
+  task: string = ''
+  tasks: string[] = []
+  isStriked: boolean[] = []
+
+  insertTask() {
+    if (this.task.trim() !== '') {
+      this.tasks.push(this.task)
+      this.isStriked.push(false)
+      this.task = ""
+    }
+  }
+
+  removeTask(indice: number) {
+    this.tasks.splice(indice, 1)
+  }
+
+  removeAllTasks() {
+    this.tasks = []
+  }
+
+  completedTask(index: number) {
+    console.log(this.isStriked[index])
+    this.isStriked[index] = !this.isStriked[index];
+  }
+
 }
+
